77f9551a0feffe141ff5ea5632f3ecbc0cfb91b8
Veeteq <witold.wojnarowicz@gmail.com>
2023-04-20 19:01:43 +0200

    CR-005: Add HTML Parser

--- src/main/java/com/veeteq/finance/bankdocument/fileupload/UploadProcessorFactory.java
+++ src/main/java/com/veeteq/finance/bankdocument/fileupload/UploadProcessorFactory.java
@@ -2,14 +2,15 @@ package com.veeteq.finance.bankdocument.fileupload;
 
 public class UploadProcessorFactory {
 
-	public static UploadProcessor getUploadProcessor(String fileType) throws IllegalArgumentException {
-		switch (fileType) {
-		case "application/pdf":
-			return new CitiBankProcessor();
-
-		default:
-			throw new IllegalArgumentException("No processor for selected value");
-		}
-	}
+  public static UploadProcessor getUploadProcessor(String fileType) throws IllegalArgumentException {
+    switch (fileType) {
+    case "application/pdf":
+      return new CitiBankProcessor();
+        case "text/html":
+          return new MBankProcessor();
+    default:
+      throw new IllegalArgumentException("No processor for selected value");
+    }
+  }
 
 }
