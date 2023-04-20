3e3a050e5505c6a18969b5f854715f7e19fa9cb1
Veeteq <witold.wojnarowicz@gmail.com>
2023-04-20 18:53:50 +0200

    CR-004: Add PDF Parser

--- src/main/java/com/veeteq/finance/bankdocument/fileupload/UploadProcessorFactory.java
+++ src/main/java/com/veeteq/finance/bankdocument/fileupload/UploadProcessorFactory.java
@@ -2,12 +2,14 @@ package com.veeteq.finance.bankdocument.fileupload;
 
 public class UploadProcessorFactory {
 
-    public static UploadProcessor getUploadProcessor(String fileType) throws IllegalArgumentException {
-        switch (fileType) {
+	public static UploadProcessor getUploadProcessor(String fileType) throws IllegalArgumentException {
+		switch (fileType) {
+		case "application/pdf":
+			return new CitiBankProcessor();
 
-        default:
-          throw new IllegalArgumentException("No processor for selected value");
-        }
-    }
+		default:
+			throw new IllegalArgumentException("No processor for selected value");
+		}
+	}
 
 }
