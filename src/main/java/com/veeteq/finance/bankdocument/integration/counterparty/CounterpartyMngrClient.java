994e21c8e1688d720e9367c70a8b6c345b8bc41c
Veeteq <witold.wojnarowicz@gmail.com>
2023-05-18 23:11:14 +0200

    CR-012: Integrate with Counterparty Manager

---
+++ src/main/java/com/veeteq/finance/bankdocument/integration/counterparty/CounterpartyMngrClient.java
@@ -0,0 +1,16 @@
+package com.veeteq.finance.bankdocument.integration.counterparty;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.veeteq.finance.bankdocument.dto.BankStatementDetailDTO;
+
+@FeignClient(name = "counterparty-manager")
+public interface CounterpartyMngrClient {
+    
+    @PostMapping(path = "/api/counterparties/searchByBankData", consumes = MediaType.APPLICATION_JSON_VALUE)
+    Long searchByBankData(@RequestBody BankStatementDetailDTO bankStatementDetailDTO);
+
+}
