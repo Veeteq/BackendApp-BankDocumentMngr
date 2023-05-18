48751a2592a024ecbaf5177767baa4583d27cd10
Veeteq <witold.wojnarowicz@gmail.com>
2023-05-18 21:02:45 +0200

    CR-011: Enable Eureka Discovery Client

--- src/main/java/com/veeteq/finance/bankdocument/BankDocumentApp.java
+++ src/main/java/com/veeteq/finance/bankdocument/BankDocumentApp.java
@@ -3,11 +3,13 @@ package com.veeteq.finance.bankdocument;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.veeteq.finance.bankdocument.model")
 @EnableJpaRepositories(basePackages = "com.veeteq.finance.bankdocument.repository")
+@EnableEurekaClient
 public class BankDocumentApp {
 	
 	public static void main(String[] args) {
