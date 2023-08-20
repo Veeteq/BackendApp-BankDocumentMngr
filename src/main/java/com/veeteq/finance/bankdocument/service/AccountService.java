e624b89b69d66ece700dc2441297b3f92dcfcec2
Veeteq <witold.wojnarowicz@gmail.com>
2023-08-20 21:24:55 +0200

    CR-028: Refactor AccountService

--- src/main/java/com/veeteq/finance/bankdocument/service/AccountService.java
+++ src/main/java/com/veeteq/finance/bankdocument/service/AccountService.java
@@ -1,36 +1,13 @@
 package com.veeteq.finance.bankdocument.service;
 
+import com.veeteq.finance.bankdocument.dto.AccountDTO;
+import org.springframework.cache.annotation.Cacheable;
+
 import java.util.List;
 
-import com.veeteq.finance.bankdocument.integration.homebudget.HomeBudgetMngrAPIClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+public interface AccountService {
+  List<AccountDTO> getAll();
 
-import com.veeteq.finance.bankdocument.dto.AccountDTO;
-
-@Service
-public class AccountService {
-    private final Logger LOG = LoggerFactory.getLogger(AccountService.class);
-
-    private final HomeBudgetMngrAPIClient homeBudgetAPIClient;
-
-    @Autowired
-    public AccountService(HomeBudgetMngrAPIClient homeBudgetAPIClient) {
-        this.homeBudgetAPIClient = homeBudgetAPIClient;
-    }
-
-    public List<AccountDTO> getAll() {
-        List<AccountDTO> list = homeBudgetAPIClient.getAccounts();
-        return list;
-    }
-
-    public AccountDTO getById(Long id) {
-        LOG.info("Requesting Account from external service with id: " + id);
-
-        AccountDTO account = homeBudgetAPIClient.getAccountById(id);
-        return account;
-    }
-    
+  @Cacheable(value = "accounts")
+  AccountDTO getById(Long id);
 }
